c38bdd9d45952fff0be8f08cc70cdf00675ecff7
noglik <yaroslaubuk@gmail.com>
2022-06-09 02:00:15 +0200

    Add proper status and error handling
    `lost` replaced with `status`. Possible status:
    - Configuration
    - InProgress
    - Won
    - Lost
    Cannot win yet though=(.
    Add basic `GameError` with lonely `IncorrectStatus` option.

--- src/core.rs
+++ src/core.rs
@@ -1,4 +1,36 @@
+use core::fmt;
 use std::collections::HashSet;
+
+#[derive(Debug, PartialEq, Eq)]
+enum GameError {
+    IncorrectStatus(Status, Status),
+}
+
+impl fmt::Display for GameError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            GameError::IncorrectStatus(given_status, corr_status) => write!(
+                f,
+                "game in status {:?}, but should be in {:?}",
+                given_status, corr_status
+            ),
+        }
+    }
+}
+
+#[derive(PartialEq, Copy, Clone, Eq)]
+enum Status {
+    Configuration,
+    InProgress,
+    Won,
+    Lost,
+}
+
+impl fmt::Debug for Status {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 struct Position(usize, usize);
@@ -8,7 +40,7 @@ pub struct Game {
     height: usize,
     mine_positions: HashSet<Position>,
     open_positions: HashSet<Position>,
-    lost: bool,
+    status: Status,
 }
 
 impl Game {
@@ -18,20 +50,46 @@ impl Game {
             height,
             mine_positions: HashSet::new(),
             open_positions: HashSet::new(),
-            lost: false,
+            status: Status::Configuration,
         }
     }
 
-    fn set_mine_position(&mut self, position: Position) -> () {
+    fn set_mine_position(&mut self, position: Position) -> Result<(), GameError> {
+        if self.status != Status::Configuration {
+            return Err(GameError::IncorrectStatus(
+                self.status,
+                Status::Configuration,
+            ));
+        }
+
         self.mine_positions.insert(position);
+        Ok(())
     }
 
-    fn open(&mut self, position: Position) -> () {
+    fn start(&mut self) -> Result<(), GameError> {
+        if self.status != Status::Configuration {
+            return Err(GameError::IncorrectStatus(
+                self.status,
+                Status::Configuration,
+            ));
+        }
+
+        self.status = Status::InProgress;
+        Ok(())
+    }
+
+    fn open(&mut self, position: Position) -> Result<(), GameError> {
+        if self.status != Status::InProgress {
+            return Err(GameError::IncorrectStatus(self.status, Status::InProgress));
+        }
+
         if self.mine_positions.contains(&position) {
-            self.lost = true;
+            self.status = Status::Lost;
         } else {
             self.open_positions.insert(position);
         }
+
+        Ok(())
     }
 }
 
@@ -47,7 +105,7 @@ mod tests {
         assert_eq!(game.height, 100);
         assert_eq!(game.mine_positions.len(), 0);
         assert_eq!(game.open_positions.len(), 0);
-        assert_eq!(game.lost, false);
+        assert_eq!(game.status, Status::Configuration);
     }
 
     #[test]
@@ -56,9 +114,61 @@ mod tests {
 
         let mine_position = Position(1, 1);
 
-        game.set_mine_position(mine_position);
+        game.set_mine_position(mine_position).expect("Set mine");
 
         assert!(game.mine_positions.contains(&mine_position));
+    }
+
+    #[test]
+    fn set_mine_in_progress_game() {
+        let mut game = Game::new(10, 10);
+
+        game.start().expect("Game started");
+
+        assert_eq!(
+            game.set_mine_position(Position(1, 1)),
+            Err(GameError::IncorrectStatus(
+                Status::InProgress,
+                Status::Configuration
+            ))
+        );
+    }
+
+    #[test]
+    fn start_fresh_game() {
+        let mut game = Game::new(10, 10);
+
+        game.start().expect("Game started");
+
+        assert_eq!(game.status, Status::InProgress);
+    }
+
+    #[test]
+    fn start_already_started_game() {
+        let mut game = Game::new(1, 1);
+
+        game.start().expect("Game started");
+
+        assert_eq!(
+            game.start(),
+            Err(GameError::IncorrectStatus(
+                Status::InProgress,
+                Status::Configuration
+            ))
+        );
+    }
+
+    #[test]
+    fn open_in_config_game() {
+        let mut game = Game::new(1, 1);
+
+        assert_eq!(
+            game.open(Position(1, 1)),
+            Err(GameError::IncorrectStatus(
+                Status::Configuration,
+                Status::InProgress
+            ))
+        );
     }
 
     #[test]
@@ -68,10 +178,12 @@ mod tests {
         let mine_position = Position(1, 1);
         let safe_position = Position(1, 2);
 
-        game.set_mine_position(mine_position);
-        game.open(safe_position);
+        game.set_mine_position(mine_position).expect("Set mine");
+        game.start().expect("Game started");
 
-        assert_eq!(game.lost, false);
+        game.open(safe_position).expect("Position opened");
+
+        assert_eq!(game.status, Status::InProgress);
         assert!(game.open_positions.contains(&safe_position));
     }
 
@@ -81,9 +193,11 @@ mod tests {
 
         let mine_position = Position(1, 1);
 
-        game.set_mine_position(mine_position);
-        game.open(mine_position);
+        game.set_mine_position(mine_position).expect("Set mine");
+        game.start().expect("Game started");
 
-        assert!(game.lost);
+        game.open(mine_position).expect("Position opened");
+
+        assert_eq!(game.status, Status::Lost);
     }
 }
